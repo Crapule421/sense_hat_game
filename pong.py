0768ab6cda0dd418af187e8c3d626767118afff0
Ressort Aloïs <ressort.alois@gmail.com>
2018-12-12 09:29:18 +0100

    Detection action joystick

--- pong.py
+++ pong.py
@@ -1,9 +1,9 @@
 #Initialisation du SenseHat
- from sense_hat import SenseHat
-  sense = SenseHat()
+from sense_hat import SenseHat
+sense = SenseHat()
 
 #Si le SenseHat est bien initialisé alors je récupère les informations
 #du joystick et les print en console.
-  while True:
-      for event in sense.stick.get_events():
-          print(event.direction, event.action)
+while True:
+    for event in sense.stick.get_events():
+        print(event.direction, event.action)
